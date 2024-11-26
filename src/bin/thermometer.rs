dd7d8778954c2369e59b8391f73468e5f1f0abe5
Luca Barbato <lu_zero@gentoo.org>
2024-11-26 02:24:06 +0100

    refactor: Move the common code to lib.rs

--- src/bin/thermometer.rs
+++ src/bin/thermometer.rs
@@ -8,7 +8,6 @@ use alloc::{
     format,
     string::{String, ToString},
 };
-use const_random::const_random;
 use embassy_executor::Spawner;
 use embassy_net::{Stack, StackResources};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex, watch::Watch};
@@ -25,10 +24,7 @@ use esp_hal::{
 use esp_println::println;
 use esp_wifi::{
     init,
-    wifi::{
-        ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
-        WifiState,
-    },
+    wifi::{WifiDevice, WifiStaDevice},
     EspWifiController,
 };
 use picoserve::{
@@ -37,8 +33,9 @@ use picoserve::{
     routing::get,
 };
 use shtcx::{self, sensor_class::Sht2Gen, shtc3, PowerMode, ShtCx};
-use uuid::Builder;
 use wot_td::{builder::*, Thing};
+
+use wot_esp_hal_demo::*;
 
 #[derive(Clone, Copy)]
 struct AppState {
@@ -75,8 +72,6 @@ impl AppState {
 type AppRouter = impl picoserve::routing::PathRouter<AppState>;
 
 const WEB_TASK_POOL_SIZE: usize = 1;
-
-const UUID_SEED: [u8; 16] = const_random!([u8; 16]);
 
 #[embassy_executor::task(pool_size = WEB_TASK_POOL_SIZE)]
 async fn web_task(
@@ -127,23 +122,6 @@ async fn temperature_write_task(state: &'static AppState) -> ! {
             }
         }
     }
-}
-
-// https://github.com/embassy-rs/static-cell/issues/16
-macro_rules! mk_static {
-    ($t:ty,$val:expr) => {{
-        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
-        STATIC_CELL.init_with(|| $val)
-    }};
-}
-
-const SSID: &str = env!("SSID");
-const PASSWORD: &str = env!("PASSWORD");
-
-fn generate_uuid_urn() -> alloc::string::String {
-    let uuid = Builder::from_random_bytes(UUID_SEED).into_uuid();
-
-    uuid.urn().to_string()
 }
 
 static WATCH: Watch<CriticalSectionRawMutex, f32, 2> = Watch::new();
@@ -260,12 +238,7 @@ async fn main(spawner: Spawner) {
 
     let sht = mk_static!(ShtCx<Sht2Gen, &'static mut I2c<'static, Blocking, AnyI2c>>, shtc3(i2c));
 
-    let id = if cfg!(feature = "uuid-id") {
-        generate_uuid_urn()
-    } else {
-        let device_id = stack.hardware_address().to_string();
-        format!("urn:example/shtc3/{device_id}")
-    };
+    let id = get_urn_or_uuid(stack);
 
     let td = Thing::builder("shtc3")
         .finish_extend()
@@ -403,42 +376,4 @@ async fn main(spawner: Spawner) {
     }
 
     spawner.spawn(temperature_write_task(app_state)).ok();
-}
-
-#[embassy_executor::task]
-async fn connection(mut controller: WifiController<'static>) {
-    println!("start connection task");
-    println!("Device capabilities: {:?}", controller.capabilities());
-    loop {
-        if esp_wifi::wifi::wifi_state() == WifiState::StaConnected {
-            // wait until we're no longer connected
-            controller.wait_for_event(WifiEvent::StaDisconnected).await;
-            Timer::after(Duration::from_millis(5000)).await
-        }
-        if !matches!(controller.is_started(), Ok(true)) {
-            let client_config = Configuration::Client(ClientConfiguration {
-                ssid: SSID.try_into().unwrap(),
-                password: PASSWORD.try_into().unwrap(),
-                ..Default::default()
-            });
-            controller.set_configuration(&client_config).unwrap();
-            println!("Starting wifi");
-            controller.start_async().await.unwrap();
-            println!("Wifi started!");
-        }
-        println!("About to connect...");
-
-        match controller.connect_async().await {
-            Ok(_) => println!("Wifi connected!"),
-            Err(e) => {
-                println!("Failed to connect to wifi: {e:?}");
-                Timer::after(Duration::from_millis(5000)).await
-            }
-        }
-    }
-}
-
-#[embassy_executor::task]
-async fn net_task(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
-    stack.run().await
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,81 @@
 #![no_std]
 
+extern crate alloc;
+
+use alloc::{
+    format,
+    string::{String, ToString},
+};
+use embassy_net::{driver::Driver, Stack};
+use embassy_time::{Duration, Timer};
+use esp_println::println;
+use esp_wifi::wifi::{
+    ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
+    WifiState,
+};
+
 pub mod smartled;
+
+// https://github.com/embassy-rs/static-cell/issues/16
+#[macro_export]
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        STATIC_CELL.init_with(|| $val)
+    }};
+}
+
+pub const SSID: &str = env!("SSID");
+pub const PASSWORD: &str = env!("PASSWORD");
+
+const UUID_SEED: [u8; 16] = const_random::const_random!([u8; 16]);
+
+/// Produce an urn that can be used as id
+pub fn get_urn_or_uuid<T: Driver>(stack: &Stack<T>) -> String {
+    if cfg!(feature = "uuid-id") {
+        let uuid = uuid::Builder::from_random_bytes(UUID_SEED).into_uuid();
+
+        uuid.urn().to_string()
+    } else {
+        let device_id = stack.hardware_address().to_string();
+        format!("urn:example/shtc3/{device_id}")
+    }
+}
+
+#[embassy_executor::task]
+pub async fn connection(mut controller: WifiController<'static>) {
+    println!("start connection task");
+    println!("Device capabilities: {:?}", controller.capabilities());
+    loop {
+        if esp_wifi::wifi::wifi_state() == WifiState::StaConnected {
+            // wait until we're no longer connected
+            controller.wait_for_event(WifiEvent::StaDisconnected).await;
+            Timer::after(Duration::from_millis(5000)).await
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = Configuration::Client(ClientConfiguration {
+                ssid: SSID.try_into().unwrap(),
+                password: PASSWORD.try_into().unwrap(),
+                ..Default::default()
+            });
+            controller.set_configuration(&client_config).unwrap();
+            println!("Starting wifi");
+            controller.start_async().await.unwrap();
+            println!("Wifi started!");
+        }
+        println!("About to connect...");
+
+        match controller.connect_async().await {
+            Ok(_) => println!("Wifi connected!"),
+            Err(e) => {
+                println!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+pub async fn net_task(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
+    stack.run().await
+}
